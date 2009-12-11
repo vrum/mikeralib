451837d1d9a36e1387e5c9b219564eaaffae7887
mike_r_anderson@yahoo.co.uk <mike_r_anderson@yahoo.co.uk@a26a99da-b995-11dd-a80a-757f205f25d6>
2009-12-11 08:36:58 +0000

    Uber-cool RayCaster implementation

--- trunk/Mikera/src/mikera/engine/RayCaster.java
+++ trunk/Mikera/src/mikera/engine/RayCaster.java
@@ -3,11 +3,30 @@ package mikera.engine;
 import mikera.math.*;
 import mikera.util.Bits;
 import mikera.util.Maths;
+import mikera.util.Rand;
 
 import java.util.*;
 
+/**
+ * Approximate fast ray casting for 3d grids
+ * 
+ * 1. Visits each cell once
+ * 2. Each cell assumed to have exactly one source
+ * 3. Halts when the CastFunction visitor returns false
+ * 
+ * Not intended to be visually accurate, but should
+ * perform well for locality searching / detection etc.
+ * 
+ * @author Mike Anderson
+ *
+ */
 public class RayCaster {
+	public static final int TOTAL_MAX_RANGE=100;
+	public static final int DEFAULT_MAX_RANGE=10;
+	
 	private CastFunction castFunction;
+	private int maxRange=DEFAULT_MAX_RANGE;
+	
 	
 	public static abstract class CastFunction {
 		public abstract boolean visit(int x, int y, int z);
@@ -23,8 +42,8 @@ public class RayCaster {
 	
 	
 	
-	public void cast(int cx, int cy, int cz) {
-		castLocal(cx,cy,cy,0,0,0);
+	public void cast(int startX, int startY, int startZ) {
+		castLocal(startX,startY,startY,0,0,0);
 	}
 	
 	private void castLocal(int cx, int cy, int cz, int dx, int dy, int dz) {
@@ -35,6 +54,9 @@ public class RayCaster {
 		int ax=Maths.abs(dx);
 		int ay=Maths.abs(dy);
 		int az=Maths.abs(dz);
+		if ((ax>=maxRange)||(ay>=maxRange)||(az>=maxRange)) {
+			return;
+		}
 		byte dirx=((dx>=0)?Dir.E:Dir.W);
 		byte diry=((dy>=0)?Dir.N:Dir.S);
 		byte dirz=((dz>=0)?Dir.U:Dir.D);
@@ -43,8 +65,8 @@ public class RayCaster {
 		if ((dx==0)&&(dy==0)&&(dz==0)) {
 			dirs=Dir.ALL_DIRS_MASK;
 		} else {
+			// process in direction(s) with maximum length along axis
 			if ((ax>=ay)&&(ax>=az)) dirs|=getDir(ax,ay,az,dirx,diry,dirz);
-			
 			if ((ay>=ax)&&(ay>=az)) dirs|=getDir(ay,ax,az,diry,dirx,dirz);
 			if ((az>=ax)&&(az>=ay)) dirs|=getDir(az,ax,ay,dirz,dirx,diry);
 		}
@@ -60,6 +82,9 @@ public class RayCaster {
 	private int getDir(int a1, int a2, int a3, byte d1, byte d2, byte d3) {
 		int dirs=0;
 		dirs|=getDir2(a1,a2,a3,d1,d2,d3);
+
+		// There are potentially up to 4 quadrants to consider
+		// if point is on a2 or a3 axis
 		if (a2==0) {
 			dirs|=getDir2(a1,a2,a3,d1,Dir.reverse(d2),d3);
 		}
@@ -70,26 +95,46 @@ public class RayCaster {
 			dirs|=getDir2(a1,a2,a3,d1,Dir.reverse(d2),Dir.reverse(d3));
 		}
 
-
 		return dirs;
 	}
 	
+	private static final int[] splitPoints=new int[TOTAL_MAX_RANGE];
+	
 	private int getDir2(int a1, int a2, int a3, byte d1, byte d2, byte d3) {
 		// point at which split occurs (distance from a1 axis)
-		int splitPoint=Bits.roundUpToPowerOfTwo(a1+2)-2-a1;
+		// note this can be at any point in range 0 to a1
+		// but must be a function of a1 only
+		int splitPoint;
+		//splitPoint=Bits.roundUpToPowerOfTwo(a1+2)-2-a1;
+		//splitPoint=Maths.mod(Rand.xorShift32(a1),a1);
+		splitPoint=splitPoints[a1];
+		
 		int dirs=0;
 		
 		// straight ahead
-		if (a2<=splitPoint) dirs|=Dir.dirMask(d1);
-		if (a3<=splitPoint) dirs|=Dir.dirMask(d1);
+		if ((a2<=splitPoint)&&(a3<=splitPoint)) dirs|=Dir.dirMask(d1);
 		
 		// fan out
-		if (a2>=splitPoint) dirs|=Dir.dirMask(d1+d2);
-		if (a3>=splitPoint) dirs|=Dir.dirMask(d1+d3);
+		if ((a2>=splitPoint)&&(a3<=splitPoint)) dirs|=Dir.dirMask(d1+d2);
+		if ((a3>=splitPoint)&&(a2<=splitPoint)) dirs|=Dir.dirMask(d1+d3);
 	
 		// diagonal
 		if ((a3>=splitPoint)&&(a2>=splitPoint)) dirs|=Dir.dirMask(d1+d2+d3);
 		
 		return dirs;
+	}
+
+	public void setMaxRange(int maxRange) {
+		this.maxRange = maxRange;
+	}
+
+	public int getMaxRange() {
+		return maxRange;
+	}
+	
+	static {
+		for (int i=1; i<splitPoints.length; i++) {
+			splitPoints[i]=Maths.mod(Rand.xorShift32(i),i);
+		}
 	}
 }
