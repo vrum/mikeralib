daa0eb7f184f6caaea100f6a6ad73aa38d516a1f
mike.r.anderson.13@gmail.com <mike.r.anderson.13@gmail.com@a26a99da-b995-11dd-a80a-757f205f25d6>
2011-11-29 15:52:06 +0000

    Sensors and test dependencies

--- trunk/Mikera/src/test/java/mikera/ai/TestSensor.java
+++ trunk/Mikera/src/test/java/mikera/ai/TestSensor.java
@@ -15,7 +15,7 @@ public class TestSensor {
 		double[] ds=new double[8];
 		
 		c.sense(' ',ds);
-		assertArrayEquals(ds,new double[] {-1,-1,-1,-1,-1,1,-1,-1},0.00001);
+		assertArrayEquals(ds,new double[] {0,0,0,0,0,1,0,0},0.00001);
 
 		try {
 			c.sense(null,ds);
@@ -35,7 +35,7 @@ public class TestSensor {
 		ds[32]=10;
 		
 		ss.sense(" ",ds);
-		assertEquals(-1,ds[0],0.00001);
+		assertEquals(0,ds[0],0.00001);
 		assertEquals(1,ds[5],0.00001);
 		assertEquals(0,ds[8],0.00001);
 		assertEquals(0,ds[31],0.00001);
@@ -44,11 +44,11 @@ public class TestSensor {
 		ds[31]=10;
 		ds[32]=10;
 		ss.sense("        ",ds);
-		assertEquals(-1,ds[0],0.00001);
+		assertEquals(0,ds[0],0.00001);
 		assertEquals(1,ds[5],0.00001);
-		assertEquals(-1,ds[8],0.00001);
+		assertEquals(0,ds[8],0.00001);
 		assertEquals(1,ds[13],0.00001);
-		assertEquals(-1,ds[31],0.00001);
+		assertEquals(0,ds[31],0.00001);
 		assertEquals(10,ds[32],0.00001);
 
 		ss.sense("",ds);
