a7e4a6e154748fc8ab3e364319e3f2a476b21cf2
mike.r.anderson.13@gmail.com <mike.r.anderson.13@gmail.com@a26a99da-b995-11dd-a80a-757f205f25d6>
2012-03-07 05:12:22 +0000

    Array operation refactoring

--- trunk/Mikera/src/test/java/mikera/test/TestVector.java
+++ trunk/Mikera/src/test/java/mikera/test/TestVector.java
@@ -1,60 +1,28 @@
 package mikera.test;
 
-import static org.junit.Assert.assertEquals;
-import mikera.image.Colours;
-import mikera.math.Matrix;
-import mikera.math.Vector;
-import mikera.util.Rand;
+import static org.junit.Assert.*;
+import mikera.vector.Vector;
 
 import org.junit.Test;
 
-public class TestVector {
-	@Test public void testInit() {
-		Vector v=new Vector(0,1,2);
-		v.scale(2);
-		assertEquals("{0.0, 2.0, 4.0}",v.toString());
-	}
-	
 
-	@Test public void testNormalise() {
-		Vector v=new Vector(6,0,0);
+public class TestVector {
+	@Test public void testSubVectors() {
+		double[] data=new double[100];
+		for (int i=0; i<100; i++) data[i]=i;
 		
-		float len=v.normalise();
-		assertEquals("{1.0, 0.0, 0.0}",v.toString());
-		assertEquals(6,len,0.0001f);
+		Vector v=new Vector(data);
+		assertEquals(10,v.get(10),0.0);
+		
+		Vector v2=v.subVector(5, 90);
+		assertEquals(90,v2.length);
+		assertEquals(15,v2.get(10),0.0);
+		
+		Vector v3=v2.subVector(5,80);
+		assertEquals(20,v3.get(10),0.0);
+		
+		v3.set(10, -99);
+		assertEquals(-99,v.get(20),0.0);
+
 	}
-	
-	@Test public void testRotation() {
-		Vector v=new Vector(1,0,0);
-		Vector v2=new Vector(3,0,0);
-		
-		Matrix m=new Matrix(3,3);
-		
-		m.setToRotation3(Rand.nextFloat()-0.5f, Rand.nextFloat()-0.5f, Rand.nextFloat()-0.5f, Rand.nextFloat()*10-5);
-		
-		Matrix.multiplyVector(m,v,v2);
-		
-		assertEquals(1.0f,v2.length(),0.0001f);
-	}
-	
-	@Test public void testColours() {
-		Vector v3=new Vector(3);
-		Vector v4=new Vector(4);
-		
-		v4.data[0]=Rand.nextFloat();
-		v4.data[1]=Rand.nextFloat();
-		v4.data[2]=Rand.nextFloat();
-		v4.data[3]=1.0f;
-		
-		v3.data[0]=v4.data[0];
-		v3.data[1]=v4.data[1];
-		v3.data[2]=v4.data[2];
-		
-		assertEquals(v4.toARGBColour(),v3.toRGBColour());
-		
-		int argb=Rand.nextInt();
-		Colours.toFloat4(v4.data, 0, argb);
-		assertEquals(argb,v4.toARGBColour());
-	}
-	
 }
