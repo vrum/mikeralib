3332ca15d15a72a8db5f84494eccec9878415c76
mike.r.anderson.13@gmail.com <mike.r.anderson.13@gmail.com@a26a99da-b995-11dd-a80a-757f205f25d6>
2012-05-17 06:43:35 +0000

    Included Resource test case

--- trunk/Mikera/src/main/java/mikera/util/Resource.java
+++ trunk/Mikera/src/main/java/mikera/util/Resource.java
@@ -6,7 +6,7 @@ import java.net.URL;
 /**
  * Utility functions for resource loading
  * 
- * Resources should be in the jar of the main class, and named as "/resources/something.res"
+ * Resources should be in the jar of the main class, and named as "resources/something.res"
  * 
  * @author Mike
  *
@@ -18,5 +18,10 @@ public class Resource {
 	
 	public static InputStream getResourceAsStream(String filename) {
 		return Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+	}
+	
+	public static String getResourceAsString(String filename) {
+		InputStream is=getResourceAsStream(filename);
+		return mikera.util.Tools.readStringFromStream(is);
 	}
 }
